aa59a0844c3c6d52c2bd6aeb19b48ad9a52eba0a
huangjunren <huangjunren@chinayaan.com>
2015-08-07 10:03:40 +0800

    Upload and Create FFmpeg's modified Source

---
+++ libavfilter/x86/vf_interlace_init.d
@@ -0,0 +1,21 @@
+libavfilter/x86/vf_interlace_init.o: libavfilter/x86/vf_interlace_init.c \
+ libavutil/attributes.h libavutil/cpu.h libavutil/attributes.h \
+ libavutil/internal.h config.h libavutil/timer.h libavutil/log.h \
+ libavutil/avutil.h libavutil/common.h libavutil/version.h \
+ libavutil/macros.h libavutil/avconfig.h libavutil/intmath.h \
+ libavutil/x86/intmath.h libavutil/mem.h libavutil/error.h \
+ libavutil/internal.h libavutil/rational.h libavutil/mathematics.h \
+ libavutil/intfloat.h libavutil/pixfmt.h libavutil/old_pix_fmts.h \
+ libavutil/x86/timer.h libavutil/cpu.h libavutil/dict.h \
+ libavutil/x86/emms.h libavutil/libm.h libavutil/mem.h \
+ libavutil/x86/asm.h libavutil/x86/cpu.h libavutil/cpu_internal.h \
+ libavfilter/interlace.h libavutil/common.h libavutil/imgutils.h \
+ libavutil/pixdesc.h libavutil/opt.h libavutil/samplefmt.h \
+ libavfilter/avfilter.h libavutil/avutil.h libavutil/dict.h \
+ libavutil/frame.h libavutil/buffer.h libavutil/log.h \
+ libavutil/samplefmt.h libavutil/pixfmt.h libavutil/rational.h \
+ libavfilter/version.h libavutil/version.h libavfilter/formats.h \
+ libavfilter/internal.h libavfilter/avfiltergraph.h libavfilter/thread.h \
+ libavfilter/version.h libavfilter/video.h libavcodec/avcodec.h \
+ libavutil/buffer.h libavutil/channel_layout.h libavcodec/version.h \
+ libavcodec/old_codec_ids.h
